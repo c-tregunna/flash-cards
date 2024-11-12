a6fd460dbc8aebb5b837d5343c006682e7540cbc
Claire <c.tregunna1980@gmail.com>
2024-11-12 15:56:29 +0000

    font style tweaks

--- subjects.js
+++ subjects.js
@@ -47,7 +47,7 @@ const hariboFacts = [
         answer: "Haribo Was Founded in 1920: The company was founded by Hans Riegel in Bonn, Germany, and its name, 'Haribo,' is a blend of the founder’s name and city—Hans Riegel Bonn."
     },
     {
-        question: "How many Gummy Bears for Haribo produce a day?",
+        question: "How many Gummy Bears do Haribo produce a day?",
         answer: "Haribo produces nearly 100 million gummy bears every day! The production process involves molding gelatin-based mixtures in trays filled with cornstarch to create detailed shapes, followed by drying, coating, and packaging."
     },
     {
